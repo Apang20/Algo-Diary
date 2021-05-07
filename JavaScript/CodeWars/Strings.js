321b03249800e1679d9b20c8787d0491a2cf352c
github username <avelonpang@gmail.com>
2021-05-07 10:22:01 -0500

    updated var

--- JavaScript/CodeWars/Strings.js
+++ JavaScript/CodeWars/Strings.js
@@ -162,8 +162,8 @@ function countNumOfStrings(arr){
     //check if string/ count 
     //return result
     
-    var count = 0;
-    for (var i = 0; i < arr.length; i++) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
         if(typeof arr[i] === "string") {
             count++
         }
