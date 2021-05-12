7583dc67a122cbd045ab4aebd8edb30f48a0cd09
github username <avelonpang@gmail.com>
2021-05-12 11:47:06 -0500

    longest palindrome

--- JavaScript/Exercises/Palindrome.js
+++ JavaScript/Exercises/Palindrome.js
@@ -19,3 +19,24 @@ function palindrome(str) {
             return char === str[str.length- i - 1]; 
         });
     }
+
+
+    
+    //Find the LONGEST palindrome 
+        // split sentence into words
+        // iterate words and collect the palindromes
+        // sort the list of palindromes by word length
+        // return the largest item in the sorted list
+    function findLongestPalindrome(sentence) {
+
+        let palindromes = [];
+        let words = sentence.split(' ');
+        for (let i = 0; i < words.length; i++) {
+            if (isPalindrome(words[i])) {
+                palindromes.push(words[i]);
+                palindromes.sort(sortAscendingByLength);
+            }
+        }
+        return palindromes.pop();
+        
+      }
