6e8b36379ef801949133fd4ed921f25bbbf23947
github username <avelonpang@gmail.com>
2021-05-14 15:30:01 -0500

    Fashion Algo - Part A

--- JavaScript/Exercises/Fashion.js
+++ JavaScript/Exercises/Fashion.js
@@ -1,22 +1,22 @@
 //Fashion Cataloge 
 let currentInventory = [
     {
-      name: 'Brunello Cucinelli',
-      shoes: [
-        {name: 'tasselled black low-top lace-up', price: 1000},
-        {name: 'tasselled green low-top lace-up', price: 1100},
-        {name: 'plain beige suede moccasin', price: 950},
-        {name: 'plain olive suede moccasin', price: 1050}
-      ]
+        name: 'Brunello Cucinelli',
+        shoes: [
+            { name: 'tasselled black low-top lace-up', price: 1000 },
+            { name: 'tasselled green low-top lace-up', price: 1100 },
+            { name: 'plain beige suede moccasin', price: 950 },
+            { name: 'plain olive suede moccasin', price: 1050 }
+        ]
     },
     {
-      name: 'Gucci',
-      shoes: [
-        {name: 'red leather laced sneakers', price: 800},
-        {name: 'black leather laced sneakers', price: 900}
-      ]
+        name: 'Gucci',
+        shoes: [
+            { name: 'red leather laced sneakers', price: 800 },
+            { name: 'black leather laced sneakers', price: 900 }
+        ]
     }
-  ];
+];
 
 /* 
 PART A
@@ -29,7 +29,11 @@ Example of a flat list in code:
 let flatList = "First line\nSecond Line\nThird Line";
 console.log(flatList);
 
+
 Psuedocode:  
+-Input: Array of two objects (with an array of shoe objects)
+=Output: String with designer's name and shoe information 
+
 -Set flatlist varialbe to empty string
 -Iterate over the array over inventory to get designerObject
 -Iterate over deisngerObject's shoe's property 
@@ -40,6 +44,21 @@ Psuedocode:
 
 function renderInventory(inventory) {
 
+    let flatlist = '';
 
+    for (let i = 0; i < inventory.length; i++) {
+        let designerObj = inventory[i];
+
+        for (let j = 0; j < designerObj.length; j++) {
+            flatlist += designerObj.name + ', ' + designerObj.shoes[j].name + ' ' + designerObj.shoes[j].price;
+
+            if (inventory.length - 1 && designerObj.length - 1) {
+                break;
+            } else {
+                flatlist += '/n';
+            }
+        }
+    }
+    return flatlist;
 
 }
