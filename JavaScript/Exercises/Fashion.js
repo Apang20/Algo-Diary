ab24c5f3f2bfdbafeef28fc892517b256929ae20
github username <avelonpang@gmail.com>
2021-05-18 14:51:54 -0500

    add tests

--- JavaScript/Exercises/Fashion.js
+++ JavaScript/Exercises/Fashion.js
@@ -185,3 +185,29 @@ function assertEqual(actual, expected, testName) {
     }  
 }
 
+//TESTS
+const currentInventory = [
+  {
+    name: 'Brunello Cucinelli',
+    shoes: [
+      {name: 'tasselled black low-top lace-up', price: 1000},
+      {name: 'tasselled green low-top lace-up', price: 1100},
+      {name: 'plain beige suede moccasin', price: 950},
+      {name: 'plain olive suede moccasin', price: 1050}
+    ]
+  },
+  {
+    name: 'Gucci',
+    shoes: [
+      {name: 'red leather laced sneakers', price: 800},
+      {name: 'black leather laced sneakers', price: 900}
+    ]
+  }
+];
+
+//let actualOutput = listAllBlackShoes(currentInventory);
+//let expectedOutput = 'Brunello Cucinelli, tasselled black low-top lace-up, 1000\nGucci, black leather laced sneakers, 900';
+//assertEqual(actualOutput, expectedOutput, 'should return a flatlist with all of the black shoes within the inventory');
+
+
+
