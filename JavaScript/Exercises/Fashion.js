0655df24a154f607c7c07ab4c9afc00287d6aa2d
github username <avelonpang@gmail.com>
2021-05-19 13:28:42 -0500

    FI Part C Tests

--- JavaScript/Exercises/Fashion.js
+++ JavaScript/Exercises/Fashion.js
@@ -300,6 +300,7 @@ function targetWordIndex(nameWordsArray) {
     }
 }
 
+
 // ASSERTION FUNCTION(S) TO BE USED
 function assertObjectsEqual(actual, expected, testName) {
     actual = JSON.stringify(actual);
@@ -310,3 +311,67 @@ function assertObjectsEqual(actual, expected, testName) {
       console.log('FAILED [' + testName + '] Expected ' + expected + ', but got ' + actual);
     }
   }
+
+
+
+  
+  // TESTS FOR LIST ALL BLACK SHOES
+let inventory = [
+    {
+      name: 'Brunello Cucinelli',
+      shoes: [
+        {name: 'tasselled black low-top lace-up', price: 1000},
+        {name: 'tasselled green low-top lace-up', price: 1100},
+        {name: 'plain beige suede moccasin', price: 950},
+        {name: 'plain olive suede moccasin', price: 1050}
+      ]
+    },
+    {
+      name: 'Gucci',
+      shoes: [
+        {name: 'red leather laced sneakers', price: 800},
+        {name: 'black leather laced sneakers', price: 900}
+      ]
+    }
+  ];
+  let actualLaceDetails = generateLaceDetails(inventory);
+  let expectedLaceDetails = [
+    {
+      "nameWords": [
+        "tasselled",
+        "black",
+        "low-top",
+        "lace-up"
+      ],
+      "targetWordIndex": 3
+    },
+    {
+      "nameWords": [
+        "tasselled",
+        "green",
+        "low-top",
+        "lace-up"
+      ],
+      "targetWordIndex": 3
+    },
+    {
+        "nameWords": [
+          "red",
+          "leather",
+          "laced",
+          "sneakers"
+        ],
+        "targetWordIndex": 2
+      },
+      {
+        "nameWords": [
+          "black",
+          "leather",
+          "laced",
+          "sneakers"
+        ],
+        "targetWordIndex": 2
+      }
+    ];
+
+    assertObjectsEqual(actualLaceDetails, expectedLaceDetails, 'should render correct lace details array');
