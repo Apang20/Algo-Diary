435b78d9da4aee177def3f2493f5e00066ca099d
github username <avelonpang@gmail.com>
2021-06-01 14:53:42 -0500

    array average

--- JavaScript/Exercises/Avg_Sort.js
+++ JavaScript/Exercises/Avg_Sort.js
@@ -1,6 +1,14 @@
 
+//Input - array of nums 
+//Output - average 
 
-
+function average(array) {
+    let total = 0
+    for (let i = 0; i < array.length; i++) {
+        total += array[i]
+    }
+    return total / array.length
+}
 
 
 
