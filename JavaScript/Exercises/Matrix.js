ea82fcf5ff6639e215f9affef50119d29cf065f4
github username <avelonpang@gmail.com>
2021-06-04 00:01:58 -0500

    matrix psuedocode

--- JavaScript/Exercises/Matrix.js
+++ JavaScript/Exercises/Matrix.js
@@ -1,22 +1,38 @@
-// --- Directions
-// Write a function that accepts an integer N
-// and returns a NxN spiral matrix.
-// --- Examples
-//   matrix(2)
-//     [[1, 2],
-//     [4, 3]]
-//   matrix(3)
-//     [[1, 2, 3],
-//     [8, 9, 4],
-//     [7, 6, 5]]
-//  matrix(4)
-//     [[1,   2,  3, 4],
-//     [12, 13, 14, 5],
-//     [11, 16, 15, 6],
-//     [10,  9,  8, 7]]
+/* 
+DIRECTIONS:
+Write a function that accepts an integer N
+and returns a NxN spiral matrix.
+--- Examples
+  matrix(2)
+    [[1, 2],
+    [4, 3]]
+  matrix(3)
+    [[1, 2, 3],
+    [8, 9, 4],
+    [7, 6, 5]]
+ matrix(4)
+    [[1,   2,  3, 4],
+    [12, 13, 14, 5],
+    [11, 16, 15, 6],
+    [10,  9,  8, 7]]
+
+PSEUDOCODE:
+- create empty results array
+- create counter variable starting at 1
+- if (startc <= endc) && (startr <= endr)
+loop from start column to end column
+- at results [start_row][i] assign counter var
+- increment counter 
+- increment start row 
+-loop from start row to end row 
+- At results[i][end_column] assign counter var 
+- increment counter 
+- decrement end row  
+- repeat for the other 2 sides  
+*/
 
 function matrix(n) {
 
-    
+
 }
 
