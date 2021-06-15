209c24c5708e41baa03e9e9aeb586b74676d55dd
github username <avelonpang@gmail.com>
2021-06-15 16:18:11 -0500

    illustrating recursion

--- JavaScript/Exercises/Recursive.js
+++ JavaScript/Exercises/Recursive.js
@@ -1,5 +1,5 @@
 
-let factorial = function fac(num){
+let factorial = function fac(num){ 
     if (num ===1){
         return 1; 
     }else{
@@ -9,3 +9,17 @@ let factorial = function fac(num){
 
 let final = factorial(5); 
 console.log(final); //120
+
+/* 
+
+5*fac(4)
+5*4*fac(3)
+5*4*3*fac(2)
+5*4*3*2*1
+5*4*3*2
+5*4*6
+5*24
+120
+
+
+*/
