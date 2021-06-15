9cf85f307773772c4fb4d1892edd053729b60db4
github username <avelonpang@gmail.com>
2021-06-15 16:25:08 -0500

    countDown

--- JavaScript/Exercises/Recursive.js
+++ JavaScript/Exercises/Recursive.js
@@ -1,16 +1,38 @@
 
-let factorial = function fac(num){ 
-    if (num ===1){
-        return 1; 
-    }else{
-        return num * fac(num -1); 
+function countDown(num) {
+    if (num > 1) {
+        countDown(num - 4);
     }
-}; 
+}
+countDown(5);
 
-let final = factorial(5); 
+
+
+function sum(elements) {
+    if (elements.length === 0) {
+        return 0;
+    }else{
+        const [head,...rest] = elements;
+        return head * sum(rest);
+    }
+}
+console.log(sum([1, 2, 3, 4, 5]));
+
+
+
+
+let factorial = function fac(num) {
+    if (num === 1) {
+        return 1;
+    } else {
+        return num * fac(num - 1);
+    }
+};
+
+let final = factorial(5);
 console.log(final); //120
 
-/* 
+/*
 
 5*fac(4)
 5*4*fac(3)
